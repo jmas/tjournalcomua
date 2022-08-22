44a5a022eb414ca118a5eb19c25f8f94b60e4d21
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-08-22 23:13:42 +0300

    Few text fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,27 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { AuthProvider } from "../context/Auth";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-      </MantineProvider>
-    </AuthProvider>
+    <>
+      <Script strategy="lazyOnload" id="google-analytics-script">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <AuthProvider>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </AuthProvider>
+    </>
   );
 }
 
